764d6363f739417c6c17756e6fa6d963511045a3
igorkonovalchik <ikonovalchik@redloft.ru>
2021-06-21 14:35:15 +0300

    start gift branch

--- assets/index.js
+++ assets/index.js
@@ -1,40 +1,8 @@
 
+// страница сертфикатов
+import './js/gift.js'
 
-// import './js/countUp.min'
-import '../node_modules/vidbacking/dist/jquery.vidbacking.min'
-import '../../plugins/pickadate/lib/compressed/picker'
-import '../../plugins/pickadate/lib/compressed/picker.date'
-import '../../plugins/pickadate/lib/compressed/picker.time'
-import '../../plugins/pickadate/lib/translations/ru_RU'
-
-// import '../node_modules/smoothscroll-for-websites/SmoothScroll'
-// import '../node_modules/readmore-js/readmore'
-
-// import '../node_modules/moment/min/locales.min'
-// import '../node_modules/moment/min/moment.min'
-
-// import '../../plugins/moment/moment'
-// import '../../plugins/moment/locales'
-// import '../../plugins/moment/ru'
-
-import './js/spaCart'
-import './js/spaPopUp'
-import './js/newSpaCart' // yclients form
-// import './js/spaform' старая форма без оплат
-import './js/mainform' // форма на главной
-import './js/dopmenu' // подменю в разделах
-import './js/spa'
-import './js/hashLink.js' // кликает кнопки по меткам в урле
-
-import './js/tlBookIntegration.js' // интеграция виджета тревелайн
-
-import '../../plugins/pickadate/lib/themes/default.date.css'
-import '../../plugins/pickadate/lib/themes/default.css'
-import '../../plugins/pickadate/lib/themes/default.time.css'
-import '../node_modules/vidbacking/dist/jquery.vidbacking.min.css'
-import './scss/spa.scss'
-import './scss/hotel.scss'
-import './scss/restaurant.scss'
+import './scss/gift.scss'
 
 
 
