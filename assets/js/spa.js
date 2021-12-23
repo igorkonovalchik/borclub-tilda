97c2daa68a004cc4c603bb8a4d842d496b1540dc
igorkonovalchik <ikonovalchik@redloft.ru>
2021-12-23 15:28:08 +0300

    add page ny

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -41,6 +41,126 @@ $(document).ready(function(){
         }
   });
   };
+
+const goConfetti = ($f) => {
+		var COLORS, Confetti, NUM_CONFETTI, PI_2, canvas, confetti, context, drawCircle, i, range, resizeWindow, xpos;
+	
+		NUM_CONFETTI = 50;
+	
+		COLORS = [[255, 247, 112], [246, 208, 97], [218, 174, 61], [246, 187, 78], [134, 109, 36]];
+	
+		PI_2 = 3 * Math.PI;
+	
+		canvas = document.getElementById("world");
+	
+		context = canvas.getContext("2d");
+	
+		window.w = 0;
+	
+		window.h = 0;
+	
+		resizeWindow = function() {
+			window.w = canvas.width = window.innerWidth;
+			return window.h = canvas.height = window.innerHeight;
+		};
+	
+		window.addEventListener('resize', resizeWindow, false);
+	
+		window.onload = function() {
+			return setTimeout(resizeWindow, 0);
+		};
+	
+		range = function(a, b) {
+			return (b - a) * Math.random() + a;
+		};
+	
+		drawCircle = function(x, y, r, style) {
+			context.beginPath();
+			context.arc(x, y, r, 0, PI_2, false);
+			context.fillStyle = style;
+			return context.fill();
+		};
+	
+		xpos = 0.5;
+	
+		document.onmousemove = function(e) {
+			return xpos = e.pageX / w;
+		};
+	
+		window.requestAnimationFrame = (function() {
+			return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
+				return window.setTimeout(callback, 1000 / 60);
+			};
+		})();
+	
+		Confetti = (function() {
+			function Confetti() {
+				this.style = COLORS[~~range(0, 5)];
+				this.rgb = "rgba(" + this.style[0] + "," + this.style[1] + "," + this.style[2];
+				this.r = ~~range(1, 4);
+				this.r2 = 2 * this.r;
+				this.replace();
+			}
+	
+			Confetti.prototype.replace = function() {
+				this.opacity = 0;
+				this.dop = 0.03 * range(1, 4);
+				this.x = range(-this.r2, w - this.r2);
+				this.y = range(-20, h - this.r2);
+				this.xmax = w - this.r;
+				this.ymax = h - this.r;
+				this.vx = range(0, 2) + 8 * xpos - 5;
+				return this.vy = 0.7 * this.r + range(-1, 1);
+			};
+	
+			Confetti.prototype.draw = function() {
+				var _ref;
+				this.x += this.vx;
+				this.y += this.vy;
+				this.opacity += this.dop;
+				if (this.opacity > 1) {
+					this.opacity = 1;
+					this.dop *= -1;
+				}
+				if (this.opacity < 0 || this.y > this.ymax) {
+					this.replace();
+				}
+				if (!((0 < (_ref = this.x) && _ref < this.xmax))) {
+					this.x = (this.x + this.xmax) % this.xmax;
+				}
+				return drawCircle(~~this.x, ~~this.y, this.r, this.rgb + "," + this.opacity + ")");
+			};
+	
+			return Confetti;
+	
+		})();
+	
+		confetti = (function() {
+			var _i, _results;
+			_results = [];
+			for (i = _i = 1; 1 <= NUM_CONFETTI ? _i <= NUM_CONFETTI : _i >= NUM_CONFETTI; i = 1 <= NUM_CONFETTI ? ++_i : --_i) {
+				_results.push(new Confetti);
+			}
+			return _results;
+		})();
+	
+		window.step = function() {
+			var c, _i, _len, _results;
+			requestAnimationFrame(step);
+			context.clearRect(0, 0, w, h);
+			_results = [];
+			for (_i = 0, _len = confetti.length; _i < _len; _i++) {
+				c = confetti[_i];
+				_results.push(c.draw());
+			}
+			return _results;
+		};
+	
+		step();
+
+		$f && $f();
+	
+	};
 	
 
 	if( $('.video-bg').length ){
@@ -53,6 +173,7 @@ $(document).ready(function(){
 			$('#mp4BgVideo').attr('src',videoFile).after(function(){
 				$('.video-bg').vidbacking().after(function(){
 					$('.video-bg video').prop('muted',true).trigger('play');
+					
 				 });
 			 });
 		});
@@ -60,14 +181,35 @@ $(document).ready(function(){
 
 	$("body").css("overflow","hidden");
 
-	setTimeout(function() {
 
-		if($('#rec193546060').length ){  $("#rec193546060").delay(150).fadeOut('slow').hide(); };
-		if($('#rec191800303').length ){ $("#rec191800303").delay(150).fadeOut('slow').hide(); };
-		if($('.preloader').length ){ $(".preloader").delay(250).hide(); };
-		$("body").css("overflow","auto");
+	const hideLoader = () =>{
 
-	 }, 500);
+	
+
+			if($('#rec193546060').length ){  $("#rec193546060").delay(150).fadeOut('slow').hide(); };
+			if($('#rec191800303').length ){ $("#rec191800303").delay(150).fadeOut('slow').hide(); };
+			if($('.preloader').length ){ $(".preloader").delay(250).hide(); };
+			$("body").css("overflow","auto");
+	
+		
+
+	}
+
+	
+	if($("#world").length){
+		hideLoader();
+		goConfetti(function(){
+			setTimeout(function() {
+			hideLoader();
+		}, 1000);
+		}); 
+	}else{
+		setTimeout(function() {
+		hideLoader();
+	}, 500);
+	};
+
+
 
 
 
@@ -141,6 +283,11 @@ if( $('#block-search').length && c.isSmall ) {
 				if($('#rec195575253').length ){  	// /bani	
 					topImportant('#rec195575253 .t396 .t396__artboard .t396__elem');
 				};		
+
+				if($('#rec394226490').length ){  	// /bani	
+					topImportant('#rec394226490 .t396 .t396__artboard .t396__elem');
+				};	
+
 		};
 
 		if( $('.video-part').length ){
