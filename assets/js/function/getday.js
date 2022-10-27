4888c20b116abc26441768f61dfc8df0cdd06bbb
igorkonovalchik <ikonovalchik@redloft.ru>
2022-10-27 17:10:48 +0300

    add test dev

--- assets/js/function/getday.js
+++ assets/js/function/getday.js
@@ -24,7 +24,7 @@ function getWeekDay(date = 0, curTime = 0, weekDays = false) {
       if(day == 'Sat' || day == 'Sun' || day == 'Thu' || day == 'Fri' || day == 'пт' || day == 'сб' || day == 'чт' || day == 'вс'){ day = 'h'; }else{ day = 'w' };
     };
 
-    if( isInPeriod( ms, '2020/12/30', '2021/01/01' ) || isInPeriod( ms, '2021/12/30', '2022/01/01' ) ){ day = 'ny' }; // если 29 декабря 2020 года 00:00 по 2 января 2021 года  00:00
+    if( isInPeriod( ms, '2022/12/24', '2023/01/08' ) || isInPeriod( ms, '2023/12/24', '2024/01/08' ) ){ day = 'ny' }; // если 29 декабря 2020 года 00:00 по 2 января 2021 года  00:00
     if( isInPeriod( ms, '2021/01/02', '2021/01/09' ) || isInPeriod( ms, '2022/01/02', '2022/01/09' ) ){ day = 'ny'; }; // если 2 января 2022 00:00 по 10 января 00:00
     if( isInPeriod( ms, '2022/02/23' ) ){ day = 'h'; }; // 23 февраля
     if( isInPeriod( ms, '2022/03/07', '2022/03/08' ) ){ day = 'h'; }; // 7, 8 марта
