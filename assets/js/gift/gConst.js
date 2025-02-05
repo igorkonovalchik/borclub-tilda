ad8acff1e51008fd12c506df4293e3c12b2797c4
igorkonovalchik <ikonovalchik@redloft.ru>
2025-02-05 07:20:02 +0300

    change  nominal limit

--- assets/js/gift/gConst.js
+++ assets/js/gift/gConst.js
@@ -43,7 +43,7 @@ export const emptyOrder = {
   devMode: '' 
 }; 
 
-export const maxPrice = '350000'; // Максимальная стоимость
+export const maxPrice = '550000'; // Максимальная стоимость
 export const minPrice = '5000'; // Минимальная стоимость
 
 export const stepPrice = '1000';  // Шаг в слайдере
